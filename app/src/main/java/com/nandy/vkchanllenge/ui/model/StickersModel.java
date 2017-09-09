ee578639eaaa4a88364180ec5edef4b034fc4964
Jana Kosar <yana.kosar@gmail.com>
2017-09-09 22:22:19 +0300

    Move stickers on touch

--- app/src/main/java/com/nandy/vkchanllenge/ui/model/StickersModel.java
+++ app/src/main/java/com/nandy/vkchanllenge/ui/model/StickersModel.java
@@ -1,14 +1,14 @@
 package com.nandy.vkchanllenge.ui.model;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
-import com.nandy.vkchanllenge.ui.StickersDialog;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Single;
-import io.reactivex.SingleEmitter;
-import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -25,7 +23,7 @@ import io.reactivex.schedulers.Schedulers;
  * Created by yana on 09.09.17.
  */
 
-public class StickersModel{
+public class StickersModel {
 
     private Context context;
     private final List<Bitmap> stickers = new ArrayList<>();
@@ -60,11 +58,45 @@ public class StickersModel{
         return stickers;
     }
 
-   public ImageView createStickerView(Bitmap bitmap){
+    public ImageView createStickerView(Bitmap bitmap) {
         ImageView imageView = new ImageView(context);
         imageView.setImageBitmap(bitmap);
         imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
 
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            private int xDelta;
+            private int yDelta;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        xDelta = x - params.leftMargin;
+                        yDelta = y - params.topMargin;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        break;
+                    case MotionEvent.ACTION_POINTER_UP:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = -250;
+                        layoutParams.bottomMargin = -250;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
         return imageView;
-   }
+    }
 }
